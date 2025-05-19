bffe87d80577af87ec85d60a0df1d8184b948022
Grigory Ivanov <grigorashekivanov2@gmail.com>
2025-05-19 14:34:21 +0300

    убрал один тест

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -14,26 +14,26 @@ async def test_write_data(async_client):
     assert response.status_code in (200, 409)
 
 
-@pytest.mark.asyncio
-async def test_check_data(async_client):
-
-    response = await async_client.get(
-        "/check_data",
-        params={
-            "phone": "+79536767195",
-        },
-    )
-    print(response.json())
-    assert response.status_code == 200
-
-    response = await async_client.get(
-        "/check_data",
-        params={
-            "phone": "879536767195",
-        },
-    )
-    print(response.json())
-    assert response.status_code == 422
+# @pytest.mark.asyncio
+# async def test_check_data(async_client):
+#
+#     response = await async_client.get(
+#         "/check_data",
+#         params={
+#             "phone": "+79536767195",
+#         },
+#     )
+#     print(response.json())
+#     assert response.status_code == 200
+#
+#     response = await async_client.get(
+#         "/check_data",
+#         params={
+#             "phone": "879536767195",
+#         },
+#     )
+#     print(response.json())
+#     assert response.status_code == 422
 
 
 @pytest.mark.asyncio
