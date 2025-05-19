99c28bfcc3ad90ed10544cb9b61ec505bcf7971b
Grigory Ivanov <grigorashekivanov2@gmail.com>
2025-05-19 14:51:56 +0300

    убрал лишний коммент

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,10 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
 import sys
-from asyncio import new_event_loop
 
 sys.path.append("src/")
 
 from app import app
-
-
-# @pytest.fixture(scope="session")
-# def event_loop():
-#     loop = new_event_loop()
-#     yield loop
-#     loop.close()
 
 
 @pytest.fixture(scope="session")
